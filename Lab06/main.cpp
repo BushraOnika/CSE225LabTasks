a4bacfb35e835ce99524828132986249b4342448
Bushra <87435687+BushraOnika@users.noreply.github.com>
2021-11-20 19:54:30 +0600

    Finally lab 6 fully done :")

--- Lab06/main.cpp
+++ Lab06/main.cpp
@@ -1,34 +1,73 @@
 #include <iostream>
 #include "UnsortedType.cpp"
+
 using namespace std;
-template <class ItemType>
-void UnsortedType<ItemType>::printList()
-{       while (listData != NULL) {
-        cout << listData->info << " ";
-        listData = listData->next;
+int main(){
+    UnsortedType<int> list1, list2, merged;
 
+    int n, v;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> v;
+        list1.InsertItem(v);
     }
-}
-int main() {
-    UnsortedType<int> m;
-    UnsortedType<int> n;
-    UnsortedType<int> arr;
-    int s;
-    //1st list
-    cin>>s;
-    for (int i = 0; i < s; ++i) {
-        int a;
-        cin>>a;
-        m.InsertItem(a);
 
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> v;
+        list2.InsertItem(v);
     }
-    //2nd list
-    cin>>s;
-    for (int i = 0; i < s; ++i) {
-        int a;
-        cin>>a;
-        n.InsertItem(a);
 
+    int value1, value2, index1=list1.LengthIs(), index2=list2.LengthIs();
+    list1.GetNextItem(value1);
+    list2.GetNextItem(value2);
+
+    while(index1>0 && index2>0){
+        if(value1>value2){
+            merged.InsertItem(value1);
+            index1--;
+            if(index1>0)
+                list1.GetNextItem(value1);
+        }
+        else if(value2>value1){
+            merged.InsertItem(value2);
+            index2--;
+            if(index2>0)
+                list2.GetNextItem(value2);
+        }
+        else{
+            merged.InsertItem(value1);
+            merged.InsertItem(value2);
+            index1--;
+            index2--;
+            if(index1>0)
+                list1.GetNextItem(value1);
+            if(index2>0)
+                list2.GetNextItem(value2);
+        }
     }
-    n.printList();
+
+    while(index1>0){
+        merged.InsertItem(value1);
+        index1--;
+        if(index1>0)
+            list1.GetNextItem(value1);
+    }
+
+    while(index2>0){
+        merged.InsertItem(value2);
+        index2--;
+        if(index2>0)
+            list2.GetNextItem(value2);
+    }
+
+    int value_merged, index_merged = merged.LengthIs()-1;
+    while(index_merged>=0){
+        merged.GetNextItem(value_merged);
+        cout << value_merged << ' ';
+        index_merged--;
+    }
+    cout << endl;
+
+    return 0;
 }
