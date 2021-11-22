af94a3855d84ea5c47cfe0c4b7bd0a1f648050df
Md. Ishtiaq Ahamed Fahim <63500913+IAFahim@users.noreply.github.com>
2021-11-22 23:17:05 +0600

    Update main.cpp

--- Lab08/main.cpp
+++ Lab08/main.cpp
@@ -1,61 +1,68 @@
 #include <iostream>
 #include"StackType.cpp"
+
 using namespace std;
 
-int main()
-{
+void balanced(string inputExpr){
+    char ch, stackContent;
+    StackType<char> st2;
+    for (int i = 0; i < inputExpr.size(); i++) {
+        ch = inputExpr[i];
+        switch (ch) {
+            case '(':
+                st2.Push(ch);
+                break;
+            case ')':
+                if (st2.IsEmpty()) {
+                    cout << "Not Balanced";
+                    return;
+                }
+                st2.Pop();
+                break;
+        }
+
+    }
+    if (st2.IsEmpty()) {
+        cout << "Balanced";
+
+    } else
+        cout << "Not Balanced";
+
+}
+
+int main() {
     StackType<int> st;
-    if(st.IsEmpty())
-    {
-       cout<<"Stack is Empty"<<endl;
+    if (st.IsEmpty()) {
+        cout << "Stack is Empty" << endl;
     }
     st.Push(5);
     st.Push(7);
     st.Push(4);
     st.Push(2);
-   if(!st.IsEmpty()){
-    cout<<"Stack is not Empty"<<endl;
-   }
-   if(!st.IsFull()){
-    cout<<"Stack is not full"<<endl;
-   }
+    if (!st.IsEmpty()) {
+        cout << "Stack is not Empty" << endl;
+    }
+    if (!st.IsFull()) {
+        cout << "Stack is not full" << endl;
+    }
 
     st.displayStack();
-    cout<<endl;
+    cout << endl;
     st.Push(3);
     st.displayStack();
 
-    if(st.IsFull()){
-    cout<<endl<<"Stack is full"<<endl;
-   }
-     st.Pop();
+    if (st.IsFull()) {
+        cout << endl << "Stack is full" << endl;
+    }
+    st.Pop();
     st.Pop();
 
     cout << st.Top() << endl;
-    char ch,stackContent;
-    StackType<char> st2;
-    string inputExpr;
-    cin>>inputExpr;
-    for (int i=0;i<inputExpr.length();i++){
-            ch = inputExpr[i];
-            switch (ch){
-                case'(':
-                    st2.Push(ch);
-                    break;
-                case ')':
-
-                       st2.Pop();
-            }
-
-        }
-        if (st2.IsEmpty()){
-            cout<<"Balanced";
-
-        }
-        else
-        cout<<"Not Balanced";
 
 
 
-   return 0;
+    string inputExpr="(())";
+    balanced(inputExpr);
+
+    return 0;
 }
